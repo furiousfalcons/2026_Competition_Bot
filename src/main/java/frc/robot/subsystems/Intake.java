4b418ecc262428faf22699803de69192b57c612f
LeAnaCason <casonleana@gmail.com>
2026-02-28 12:17:50 -0600

    intake

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase {
+
+  private final SparkMax intakeOutputMotor;
+  private final SparkMax intakeDownMotor;
+
+  public Intake() {
+    intakeDownMotor = new SparkMax(
+      Constants.INTAKE_DOWN_MOTOR_ID,
+      MotorType.kBrushless
+);
+
+    intakeOutputMotor = new SparkMax(
+        Constants.INTAKEOUTPUT_MOTOR_ID,
+        MotorType.kBrushless
+    );
+
+    intakeOutputMotor.set(0.0);
+    intakeDownMotor.set(0.0);
+  }
+
+  public void runIntake(double speed) {
+    intakeOutputMotor.set(speed);
+  }
+
+  public void stopIntake() {
+    intakeOutputMotor.set(0.0);
+  }
+  
+  public void intakeDown() {
+    intakeDownMotor.set(-0.4);
+  }
+  
+  public void stopIntakeDown() {
+    intakeDownMotor.set(0.0);
+  }
+
+}
